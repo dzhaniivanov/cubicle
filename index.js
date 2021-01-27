be8c6609530cc294e4da9468fd5eb0beed926a76
Dzhani Ivanov <djani.ivanov@abv.bg>
2021-01-27 20:11:40 +0200

    add public folder

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express=require('express');
-
+const handlebars=require('express-handlebars');
 const config=require('./config/config');
-
 const app=express();
+
+app.engine('hbs',handlebars({
+    extname:'hbs'
+}));
+
+app.set('view engine','hbs');
+
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+    res.render('home',{layout:false})
+})
 
 app.listen(config.PORT,()=>console.log(`Server is running on port ${config.PORT} port...`));
