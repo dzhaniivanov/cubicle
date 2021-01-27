4afdb1cd1ee4ab2791bd1a739aecbcba77fa2c7f
Dzhani Ivanov <djani.ivanov@abv.bg>
2021-01-27 22:39:05 +0200

    add details page

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,10 +2,16 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.render('home',{title:'browse'})
+    res.render('home', { title: 'browse' })
 });
 router.get('/create', (req, res) => {
-    res.render('create',{title:'create'})
+    res.render('create', { title: 'create' })
+});
+
+router.get('/details/:productId', (req, res) => {
+    console.log(req.params.productId);
+    res.render('details',{title:'product details'});
+
 });
 
 module.exports = router;
