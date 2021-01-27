0d56e846178eb81677a21f2f65522663ad001cb6
Dzhani Ivanov <djani.ivanov@abv.bg>
2021-01-27 20:16:21 +0200

    express config

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express=require('express');
-const handlebars=require('express-handlebars');
 const config=require('./config/config');
+const expressConfig=require('./config/express');
 const app=express();
 
-app.engine('hbs',handlebars({
-    extname:'hbs'
-}));
+expressConfig(app);
 
-app.set('view engine','hbs');
 
-app.use(express.static('public'));
 
 app.get('/',(req,res)=>{
     res.render('home',{layout:false})
